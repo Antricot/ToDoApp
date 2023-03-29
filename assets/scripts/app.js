33c7fede4a6cc703fe6556c0614a9bc92210be34
Antricot <danieloltenceanu@gmail.com>
2023-03-29 15:13:23 +0300

    Delete nu merge

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,19 @@
 const form = document.querySelector('form');
-const lists = document.querySelector('lists');
+const lists = document.querySelector('.lists');
 const input = document.querySelector('input');
 
 let todoArr = [];
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  if (input.value === '') {
+    return;
+  }
   let id = Math.random() * 1000000;
   const todo = new Todo(id, input.value);
   todoArr = [...todoArr, todo];
   renderDOM.displayData();
+  renderDOM.clearInput();
 });
 
 class Todo {
@@ -25,10 +29,21 @@ class renderDOM {
       return `
       <div class="todo">
       <p>${item.todo}</p>
-      <span><button>Delete</button></span>
+      <button class='remove'>Delete</button>
       </div>
   `;
     });
     lists.innerHTML = displayData.join(' ');
+  }
+  static clearInput() {
+    input.value = '';
+  }
+  static removeBtn() {
+    const deleteBtn = document.querySelector('.remove');
+    deleteBtn.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        return console.log('work');
+      });
+    });
   }
 }
