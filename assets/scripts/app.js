6aaee29dfe7ae22739f8e4c87afb8617d05be857
Gabriel Dicu <razvan.dicu@toptal.com>
2023-04-05 14:40:18 +0300

    add delete event

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,6 +12,7 @@ form.addEventListener('submit', (e) => {
   let id = Math.ceil(Math.random() * 1000000);
   const todo = new Todo(id, input.value);
   todoArr = [...todoArr, todo];
+
   displayData();
   clearInput();
 });
@@ -24,20 +25,37 @@ class Todo {
 }
 
 function displayData() {
+  lists.innerHTML = '';
   todoArr.map((item) => {
-    const todoToAdd = `
-      <div class="todo" id=${item.id}>
-      <p>${item.todo}</p>
-      <button class='remove' id=btn${item.id}>Delete</button>
-      </div>
-  `;
-    lists.innerHTML += todoToAdd;
-
-    const deleteBtn = document.querySelector(`#btn${item.id}`);
-    deleteBtn.addEventListener('click', (e) => {
-      //Todo: delete item from todoArr
-      return console.log('work');
+    const aux = document.createElement('div');
+    aux.id = item.id;
+    aux.addEventListener('click', (e) => {
+      e.preventDefault();
+      todoArr = todoArr.filter((todo) => todo.id !== item.id);
+      let todoDelete = document.getElementById(item.id);
+      todoDelete.remove();
     });
+
+    aux.innerHTML = ` <p>${item.todo}</p>
+      <button class='remove' id=btn${item.id}>Delete</button>`;
+
+    lists.appendChild(aux);
+
+    //   const todoToAdd = `
+    //     <div class="todo" id=${item.id}>
+    //     <p>${item.todo}</p>
+    //     <button class='remove' id=btn${item.id}>Delete</button>
+    //     </div>
+    // `;
+    //   lists.innerHTML += todoToAdd;
+    //   const deleteBtn = document.querySelector(`#btn${item.id}`);
+
+    //   deleteBtn.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     todoArr = todoArr.filter((todo) => todo.id !== item.id);
+    //     let todoDelete = document.getElementById(item.id);
+    //     todoDelete.remove();
+    //   });
   });
 }
 
